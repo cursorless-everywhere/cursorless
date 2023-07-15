24a304b71928b89f7ff17f8bc3f019c9ef09b6f2
Phil Cohen <phillip@phillip.io>
2023-07-15 11:34:45 -0700

    constants: use ~/.cursorless-new for now, to ease switching between the old and new versions

--- packages/cursorless-vscode/src/sidecar/constants.ts
+++ packages/cursorless-vscode/src/sidecar/constants.ts
@@ -4,4 +4,4 @@ import * as os from "os";
 /**
  * The directory where we store everything for Cursorless Everywhere related things.
  */
-export const CURSORLESS_ROOT_DIRECTORY = path.join(os.homedir(), ".cursorless");
+export const CURSORLESS_ROOT_DIRECTORY = path.join(os.homedir(), ".cursorless-new");
